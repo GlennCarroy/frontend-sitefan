971e0fa58be263708fcfdba1f781d59da6f3210f
GlennCarroy <g_carroy@live.be>
2018-02-05 15:25:44 +0100

    final version without description of the gallery

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 	      "assets/image/comics.jpg"
 	    , "assets/image/batmanNolan.jpg"
 	    , "assets/image/batman.jpg"
-	  ], {duration: 5000, fade: 1000});
+	  ], {duration: 4000, fade: 1000});
 
 
 
@@ -118,3 +118,65 @@ $(document).ready(function () {
         });
 
       });
+
+
+$(document).ready(function () {
+        var carousel = $("#carousel").waterwheelCarousel({
+          flankingItems: 3,
+          movingToCenter: function ($item) {
+            $('#callback-output').prepend('movingToCenter: ' + $item.attr('id') + '<br/>');
+          },
+          movedToCenter: function ($item) {
+            $('#callback-output').prepend('movedToCenter: ' + $item.attr('id') + '<br/>');
+          },
+          movingFromCenter: function ($item) {
+            $('#callback-output').prepend('movingFromCenter: ' + $item.attr('id') + '<br/>');
+          },
+          movedFromCenter: function ($item) {
+            $('#callback-output').prepend('movedFromCenter: ' + $item.attr('id') + '<br/>');
+          },
+          clickedCenter: function ($item) {
+            $('#callback-output').prepend('clickedCenter: ' + $item.attr('id') + '<br/>');
+          }
+        });
+
+        $('#prev').bind('click', function () {
+          carousel.prev();
+          return false
+        });
+
+        $('#next').bind('click', function () {
+          carousel.next();
+          return false;
+        });
+
+      });
+
+
+// COMMANDE
+
+$( "#js-commande" ).click(function() {
+
+	var item1 = parseInt(document.getElementById("item1").value)*10;
+	var item2 = parseInt(document.getElementById("item2").value)*5;
+	var item3 = parseInt(document.getElementById("item3").value)*15;
+
+	var totalObject = item1+item2+item3;
+	//incluons les frais de livraisons
+	if (totalObject == 0) {
+		alert("Veuillez indiquer le nombre de produits désirer.")
+	}
+
+	else if (totalObject <= 15) {
+		totalObject += 10;
+		alert("Votre commande s'élève à "+totalObject+" euros.")
+	}
+	else if (totalObject >= 16 && totalObject <= 30) {
+		totalObject += 7;
+		alert("Votre commande s'élève à "+totalObject+" euros.")
+	}
+	else {
+		alert("Votre commande s'élève à "+totalObject+" euros.")
+	}
+});
+
